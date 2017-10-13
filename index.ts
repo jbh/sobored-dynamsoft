92c98a0e0fe1b82a5721877acccf957b76eea8a4
Josh Hall <josh@sobo.red>
2017-10-12 20:18:07 -0500

    Add dynamsoft service to module and use forRoot to set configuration

--- index.ts
+++ index.ts
@@ -1,14 +1,36 @@
-import { NgModule } from '@angular/core';
+import {ModuleWithProviders, NgModule, Optional, SkipSelf} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContainerComponent } from './container/container.component';
+import { ScanButtonComponent } from './scan-button/scan-button.component';
 
-const components = [ContainerComponent];
+import { DynamsoftService } from './dynamsoft.service';
+import { DynamsoftServiceConfig } from './dynamsoft-service-config.model';
+
+const components = [ContainerComponent, ScanButtonComponent];
 
 @NgModule({
   imports: [
     CommonModule
   ],
   exports: [components],
-  declarations: [components]
+  declarations: [components],
+  providers: [DynamsoftService]
 })
-export class DynamsoftModule { }
+export class DynamsoftModule {
+
+  constructor (@Optional() @SkipSelf() parentModule: DynamsoftModule) {
+    if (parentModule) {
+      throw new Error(
+        'DynamsoftModule is already loaded. Import it in the AppModule only');
+    }
+  }
+
+  static forRoot(config: DynamsoftServiceConfig): ModuleWithProviders {
+    return {
+      ngModule: DynamsoftModule,
+      providers: [
+        { provide: DynamsoftServiceConfig, useValue: config }
+      ]
+    };
+  }
+}
