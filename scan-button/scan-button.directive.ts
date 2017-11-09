14d3b9f57394882840bdb1fa009bfcffcff8f0a6
Josh Hall <josh@sobo.red>
2017-11-08 18:46:55 -0600

    Add ability to pass page count to scan button directive

--- scan-button/scan-button.directive.ts
+++ scan-button/scan-button.directive.ts
@@ -5,6 +5,8 @@ import { DynamsoftService } from '../dynamsoft.service';
 @Directive({ selector: '[sb-dynamsoft-scan-button]' })
 export class ScanButtonDirective {
 
+  @Input('sb-dynamsoft-scan-button') pageCount: number;
+
   constructor(private el: ElementRef, private renderer: Renderer, private dynamsoftService: DynamsoftService) { }
 
   @HostListener('click', ['$event'])
@@ -15,6 +17,7 @@ export class ScanButtonDirective {
   }
 
   acquireImage(): void {
+    this.dynamsoftService.options.transferCount = this.pageCount > 0 ? this.pageCount : -1;
     this.dynamsoftService.acquireImage();
   }
 
